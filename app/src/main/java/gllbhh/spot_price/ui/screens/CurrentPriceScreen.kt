cd79e71b43d303036c41dd0fa5ccaa34bd418ac0
Gleb Bulygin <gleb.a.bulygin@gmail.com>
2026-03-10 12:52:58 +0200

    style current price screen, move strings to res fiel

--- app/src/main/java/gllbhh/spot_price/ui/screens/CurrentPriceScreen.kt
+++ app/src/main/java/gllbhh/spot_price/ui/screens/CurrentPriceScreen.kt
@@ -1,12 +1,14 @@
 package gllbhh.spot_price.ui.screens
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.copy
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import gllbhh.spot_price.R
 import gllbhh.spot_price.viewmodel.SpotPriceViewModel
 
 @Composable
@@ -21,21 +23,45 @@ fun CurrentPriceScreen(
         contentAlignment = Alignment.Center
     ) {
 
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surface.copy(alpha = 0.8f)
+            ),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 0.dp
+            )
         ) {
 
-            Text(
-                text = "Current electricity price",
-                style = MaterialTheme.typography.headlineMedium
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(32.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = stringResource(R.string.price_now),
+                    style = MaterialTheme.typography.displayMedium,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
 
-            Spacer(modifier = Modifier.height(24.dp))
+                Spacer(modifier = Modifier.height(24.dp))
 
-            Text(
-                text = "$currentPrice c/kWh",
-                style = MaterialTheme.typography.displayMedium
-            )
+                Text(
+                    text = "%.2f".format(currentPrice),
+                    style = MaterialTheme.typography.displayLarge,
+                    color = MaterialTheme.colorScheme.primary
+                )
+
+                Text(
+                    text = stringResource(R.string.c_kwh),
+                    style = MaterialTheme.typography.displayMedium,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
         }
     }
 }
